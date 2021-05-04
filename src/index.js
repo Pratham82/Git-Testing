2189b22f6c242f613505a2cb24a27e9bc6d415de
Prathamesh Mali <mali.prathamesh82@gmail.com>
2021-05-04 23:57:42 +0530

    Added new comment with username

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const User = require('../models/Users')
+
+//@POST
+//Create new user
+//Created by pratham82
+const createNewUser = async (req, res) => {
+  const { name, age, designation } = req.body
+  const user = new User({
+    name,
+    age,
+    designation,
+  })
+  try {
+    const newUser = await user.save()
+    res.json({
+      status: 'Successful',
+      message: 'New user created',
+      data: {
+        newUser,
+      },
+    })
+  } catch (err) {
+    /* handle error */
+    res.json({
+      status: 'Unsuccessful',
+      message: err.message,
+    })
+  }
+}
+
+module.exports = createNewUser
