b555ea88dc7ac2c554f93c40066cacc33cf213d6
Prathamesh Mali <prathamesh.mali@mosaicwellness.in>
2022-06-01 13:30:18 +0530

    feat: 🎸 testting git cz
    ✅ Closes: 34345

--- hello.js
+++ hello.js
@@ -1,2 +1,3 @@
-const test = "dfdf"
-const test2 = "This is new line added for test"
+const test = 'dfdf'
+const test2 = 'This is new line added for test'
+const test2 = 'This is new line added for test'
